8064bace334497a36223f7d4fe3c5b1bf6a337e1
Akseli Nurmio <akseli.nurmio@yle.fi>
2024-06-10 01:10:01 +0300

    Initial app code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,146 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+  async fetch(
+    request: Request,
+    env: Env,
+    _ctx: ExecutionContext
+  ): Promise<Response> {
+    const { pathname } = new URL(request.url);
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
-	},
+    switch (pathname) {
+      case "/current": {
+        const latest =
+          (
+            await Promise.all([
+              env.map_versions.get(getTodayDate()),
+              env.map_versions.get(getYesterdayDate()),
+            ])
+          ).find((v) => v) || null;
+
+        return Response.json({
+          current_map_version: latest,
+        });
+      }
+      default:
+        return Response.json({ error: "Not found" }, { status: 404 });
+    }
+  },
+
+  async scheduled(
+    _event: ScheduledEvent,
+    env: Env,
+    _ctx: ExecutionContext
+  ): Promise<void> {
+    let previousVersion: string | null = null;
+    try {
+      previousVersion = await env.map_versions.get(getYesterdayDate());
+    } catch (e) {
+      console.error("Error occurred while checking previous map version.", e);
+    }
+
+    let latestVersion: string;
+    try {
+      latestVersion = await fetchMapVersion(env);
+    } catch (e) {
+      console.error(e);
+
+      if (previousVersion)
+        await reportError(
+          `Error occurred while checking map version. ${e}`,
+          env
+        );
+
+      throw e;
+    }
+
+    try {
+      await env.map_versions.put(getTodayDate(), latestVersion);
+    } catch (e) {
+      console.error(e);
+      await reportError(`Error occurred while saving map version. ${e}`, env);
+      throw e;
+    }
+
+    if (!previousVersion) {
+      console.log(
+        `First time checking map version, latest version is ${latestVersion}`
+      );
+    } else if (previousVersion === latestVersion) {
+      console.log(
+        `Latest map version is the same as yesterday (${latestVersion})`
+      );
+    } else {
+      const message = `Map version changed from ${previousVersion} to ${latestVersion}`;
+
+      console.log(message);
+
+      await sendEmail("New TomTom map version available", message, env);
+    }
+  },
 };
+
+async function fetchMapVersion(env: Env): Promise<string> {
+  const res = await fetch(env.WEB_PAGE_URL, {
+    signal: AbortSignal.timeout(5000),
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch map version page, status ${res.status}`);
+  }
+
+  let body = await res.text();
+
+  // Remove HTML tags from body
+  body = body.replaceAll(/<[^>]+>/g, " ");
+
+  const versionMatch = body.match(/latest\s+map\s+version\s+is\s*(\d{4})/i);
+
+  if (!versionMatch) {
+    throw new Error("Failed to find latest map version in page");
+  }
+
+  return versionMatch[1];
+}
+
+async function sendEmail(
+  subject: string,
+  message: string,
+  env: Env
+): Promise<void> {
+  const res = await fetch("https://api.sendgrid.com/v3/mail/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${env.SENDGRID_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      from: { email: "noreply@akselinurmio.fi" },
+      subject: subject,
+      personalizations: [{ to: [{ email: env.NOTIFY_EMAIL }] }],
+      content: [{ type: "text/plain", value: message }],
+    }),
+  });
+
+  if (!res.ok) {
+    console.error(
+      `Failed to send notification email, status ${
+        res.status
+      }: ${await res.text()}`
+    );
+    throw new Error("Failed to send notification email");
+  }
+}
+
+async function reportError(message: unknown, env: Env): Promise<void> {
+  await sendEmail("Error in TomTom map version check", String(message), env);
+}
+
+function getYesterdayDate(now = new Date()) {
+  const yesterday = new Date(now);
+  yesterday.setUTCDate(now.getUTCDate() - 1);
+  return yesterday.toISOString().substring(0, 10);
+}
+
+function getTodayDate(now = new Date()) {
+  return now.toISOString().substring(0, 10);
+}
