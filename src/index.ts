5dd264bf0f21bd12ce65a33bf7f049f13f2c9c2d
Akseli Nurmio <akseli.nurmio@yle.fi>
2024-06-24 22:57:49 +0000

    Add history endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+type VersionChange = {
+  created_at: number;
+  from_version: string;
+  to_version: string;
+};
+
 export default {
   async fetch(
     request: Request,
     env: Env,
-    _ctx: ExecutionContext
+    _ctx: ExecutionContext,
   ): Promise<Response> {
     const { pathname } = new URL(request.url);
 
@@ -22,13 +28,16 @@ export default {
 <meta charset="utf-8">
 <title>TomTom Map Version</title>
 <h1>TomTom Map Version</h1>
-<a href="/v1/current">Current map version</a>`,
+<ul>
+<li><a href="/v1/current">Current map version</a></li>
+<li><a href="/v1/history">Version history</a></li>
+</ul>`,
           {
             headers: {
               "Content-Type": "text/html; charset=utf-8",
               "Cache-Control": "no-cache",
             },
-          }
+          },
         );
 
       case "/current":
@@ -41,17 +50,43 @@ export default {
         });
 
       case "/v1/current": {
-        const todaysVersionPromise = env.map_versions.get(getTodayDate());
-        const yesterdaysVersionPromise = env.map_versions.get(
-          getYesterdayDate()
-        );
+        const versionWithDate: { date: string; version: string } | undefined =
+          await (async () => {
+            const shouldTodaysVersionBeChecked = new Date().getUTCHours() >= 12;
+
+            if (shouldTodaysVersionBeChecked) {
+              const today = getTodayDate();
+              const todaysVersion = await env.map_versions.get(today);
+              if (todaysVersion) return { date: today, version: todaysVersion };
+            }
+
+            const yesterday = getYesterdayDate();
+            const yesterdaysVersion = await env.map_versions.get(yesterday);
+            if (yesterdaysVersion)
+              return { date: yesterday, version: yesterdaysVersion };
+          })();
 
         return Response.json(
           {
-            current_map_version:
-              (await todaysVersionPromise) || (await yesterdaysVersionPromise),
+            current_map_version: versionWithDate?.version ?? null,
+            last_checked: versionWithDate?.date ?? null,
           },
-          { headers: { "Cache-Control": "no-cache" } }
+          { headers: { "Cache-Control": "no-cache" } },
+        );
+      }
+
+      case "/v1/history": {
+        const changes = await env.map_version_changes.list<VersionChange>();
+
+        return Response.json(
+          {
+            version_history: changes.keys.map((entry) => ({
+              date: entry.name,
+              from_version: entry.metadata?.from_version || null,
+              to_version: entry.metadata?.to_version || null,
+            })),
+          },
+          { headers: { "Cache-Control": "no-cache" } },
         );
       }
 
@@ -61,7 +96,7 @@ export default {
           {
             status: 404,
             headers: { "Cache-Control": "no-cache" },
-          }
+          },
         );
     }
   },
@@ -69,15 +104,13 @@ export default {
   async scheduled(
     _event: ScheduledEvent,
     env: Env,
-    _ctx: ExecutionContext
+    _ctx: ExecutionContext,
   ): Promise<void> {
     let previousVersion: string | null = null;
     try {
-      const todaysVersionPromise = env.map_versions.get(getTodayDate());
-      const yesterdaysVersionPromise = env.map_versions.get(getYesterdayDate());
-
       previousVersion =
-        (await todaysVersionPromise) || (await yesterdaysVersionPromise);
+        (await env.map_versions.get(getTodayDate())) ||
+        (await env.map_versions.get(getYesterdayDate()));
     } catch (e) {
       console.error("Error occurred while checking previous map version.", e);
     }
@@ -88,11 +121,12 @@ export default {
     } catch (e) {
       console.error(e);
 
-      if (previousVersion)
+      if (previousVersion) {
         await reportError(
           `Error occurred while checking map version. ${e}`,
-          env
+          env,
         );
+      }
 
       throw e;
     }
@@ -107,16 +141,28 @@ export default {
 
     if (!previousVersion) {
       console.log(
-        `First time checking map version, latest version is ${latestVersion}`
+        `First time checking map version, latest version is ${latestVersion}`,
       );
     } else if (previousVersion === latestVersion) {
       console.log(
-        `Latest map version is the same as in previous check (${latestVersion})`
+        `Latest map version is the same as in previous check (${latestVersion})`,
       );
     } else {
       const message = `Map version changed from ${previousVersion} to ${latestVersion}`;
 
       console.log(message);
+
+      const change: VersionChange = {
+        created_at: Date.now(),
+        from_version: previousVersion,
+        to_version: latestVersion,
+      };
+
+      await env.map_version_changes.put(
+        getTodayDate(),
+        JSON.stringify(change),
+        { metadata: change },
+      );
 
       await sendEmail("New TomTom map version available", message, env);
     }
@@ -149,7 +195,7 @@ async function fetchMapVersion(env: Env): Promise<string> {
 async function sendEmail(
   subject: string,
   message: string,
-  env: Env
+  env: Env,
 ): Promise<void> {
   const res = await fetch("https://api.sendgrid.com/v3/mail/send", {
     method: "POST",
@@ -170,7 +216,7 @@ async function sendEmail(
     console.error(
       `Failed to send notification email, status ${
         res.status
-      }: ${await res.text()}`
+      }: ${await res.text()}`,
     );
     throw new Error("Failed to send notification email");
   }
